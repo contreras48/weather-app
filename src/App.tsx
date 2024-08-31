4a77b33874bb78d401da4e54003b3fcf22e2d045
contreras48 <contreras4893@gmail.com>
2024-08-31 11:24:38 -0600

    chore: add .env to .gitignore and remove from tracking

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import Card from './components/card'
+
+const App: React.FC = () => {
+  const [weather, setWeather] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+
+
+  const getUserLocation = () => {
+    return new Promise<GeolocationCoordinates>((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position.coords),
+        (error) => reject(error)
+      );
+    });
+  };
+
+  const getWeather = async (lat: number, lon: number) => {
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=es`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const { latitude, longitude } = await getUserLocation();
+        const weatherData = await getWeather(latitude, longitude);
+        setWeather(weatherData);
+      } catch (err) {
+        setError('Failed to fetch weather data');
+      }
+    };
+
+    fetchWeather();
+  }, []);
+
+  return (
+    <main className="w-full h-full flex flex-col justify-center items-center bg-neutral-light">
+      {weather ? <Card weather={weather} /> : <p>Loading...</p>}
+    </main>
+  );
+}
+
+export default App;
